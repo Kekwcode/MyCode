d4bdd24dd3b4eafe3c47e07c68dd2afa90fa0687
Kekwcode <128970352+Kekwcode@users.noreply.github.com>
2023-03-26 13:19:37 +0200

    Delete Birth Date.py

--- Birth Date.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import calendar
-
-x = datetime.datetime(int(input("Enter your birth Year: ")), int(input("Enter your birth Month: ")),
-                      int(input("Enter first Date: ")))
-
-print(calendar.day_name[x.weekday()])
